e43cac4fb88bb77d4a8905e158df52b4c3878d81
unknown <julio.gonzalez7589@alumnos.udg.mx>
2021-05-25 16:51:46 -0500

    Add route to Login with google. Get user's token provided by google

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 
@@ -5,11 +6,23 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto: ${PORT}`);
-})
+});
+
+app.get("/login", (req, res) => {
+  res.render("login")
+});
+
+app.post("/login", (req, res) => {
+  const token = req.body.token
+  console.log(token)
+});
